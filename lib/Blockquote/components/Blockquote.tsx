c7b949bcd04c389d8c213aa791cb0548faf51780
Danne Lundqvist <danne@dotvoid.com>
2024-03-11 14:08:40 +0100

    Blockquote should have rounded ring when active

--- lib/Blockquote/components/Blockquote.tsx
+++ lib/Blockquote/components/Blockquote.tsx
@@ -1,8 +1,8 @@
 import { Element, type Plugin } from '@ttab/textbit'
 
 export const Blockquote = ({ children }: Plugin.ComponentProps): JSX.Element => {
-  return <Element className="py-4">
-    <div className="px-6 border-l-8 border-l-slate-200">
+  return <Element className="py-4 group">
+    <div className="px-6 border-l-8 border-l-slate-200 rounded group-data-[state='active']:ring-1 ring-offset-4 rounded-xs">
       {children}
     </div>
   </Element>
