4ef41d51b29621cb13b96478869dde10c79aa2fe
Danne Lundqvist <danne@dotvoid.com>
2024-05-10 14:09:29 +0200

    Removed debug leftover

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -187,7 +187,7 @@ function ContentMenuItem({ action }: { action: PluginRegistryAction }): JSX.Elem
       className="grid gap-x-5 py-[0.4rem] border group grid-cols-[1.5rem_minmax(max-content,_220px)_minmax(max-content,_90px)] rounded cursor-default border-white hover:border-gray-200 hover:bg-gray-100 dark:border-slate-900 dark:hover:border-slate-700 dark:hover:bg-slate-800"
     >
       <Menu.Icon className="flex justify-self-end self-center group-data-[state='active']:font-semibold" />
-      <Menu.Label className="LABEL self-center text-sm group-data-[state='active']:font-semibold" />
+      <Menu.Label className="self-center text-sm group-data-[state='active']:font-semibold" />
       <Menu.Hotkey className="justify-self-end self-center pl-6 pr-3 text-sm opacity-70" />
     </Menu.Item>
   )
