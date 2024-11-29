68b82e8dc82280b6e6a87c9fa71eb23d2f700ea2
Victor Lindh <victorlindh@hotmail.com>
2024-11-29 08:48:46 +0100

    return empty if no modified value is found

--- lib/Factbox/components/FactboxModified.tsx
+++ lib/Factbox/components/FactboxModified.tsx
@@ -1,12 +1,18 @@
+import { formatDate } from "../lib/formatDate"
+
 interface Modified {
   modified?: string | undefined | boolean | number
 }
 
 export const FactboxModified = ({ modified = '' }: Modified): JSX.Element => {
-  return (
-    <div className='py-2 ps-2 opacity-70 font-semibold text-xs flex justify-between items-center gap-1'>
-      <div>Senast ändrad</div>
-      <div>{modified}</div>
-    </div>
-  )
+  if (modified) {
+    const formatted = formatDate(modified as string)
+    return (
+      <div className='py-2 ps-2 pr-1 opacity-70 font-semibold text-xs flex justify-between items-center gap-1'>
+        <div>Senast ändrad</div>
+        <div>{formatted}</div>
+      </div>
+    )
+  }
+  return <></>
 }
