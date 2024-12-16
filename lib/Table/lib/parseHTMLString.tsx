0df7efde739a71373e7694c0cc7e3d558ff9e4ee
Victor Lindh <victorlindh@hotmail.com>
2024-12-16 14:43:26 +0100

    remove unused parse helper

--- lib/Table/lib/parseHTMLString.tsx
+++
@@ -1,7 +0,0 @@
-export function parseHTMLString (html: string): HTMLTableRowElement[] {
-  const withTableElement = `<table>${html}</table>`
-  const parser = new DOMParser()
-  const doc = parser.parseFromString(withTableElement, 'text/html')
-  const rows = Array.from(doc.querySelectorAll('tr'))
-  return rows
-}
