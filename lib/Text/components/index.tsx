3a229d7c2ba7a00522d9785b11ff4d0a75399b51
Victor Lindh <victorlindh@hotmail.com>
2025-01-03 14:23:30 +0100

    add reusable CharacterCount component to display counter that reads from 'options.countCharacters' array. Add some more minor styling and checks for conditional rendering of counter

--- lib/Text/components/index.tsx
+++ lib/Text/components/index.tsx
@@ -1,4 +1,4 @@
-import { TextbitElement, type Plugin } from '@ttab/textbit'
+import { type TBElement, TextbitElement, type Plugin } from '@ttab/textbit'
 import { getTextStyles } from '../textStyles'
 import { Node } from 'slate'
 
@@ -14,46 +14,70 @@ export const Text = ({ children, element, options, rootNode }: Plugin.ComponentP
       return (
         <div draggable={false} contentEditable={editable} className="font-sans dark:tracking-wide">
           {children}
+          <CharacterCount element={element} type='body' countTypes={options?.countCharacters as string[]} />
         </div>
       )
     }
-  
+
     return <div draggable={false} contentEditable={editable} className="py-2 font-serif dark:tracking-wide">
       {children}
+      <CharacterCount element={element} type='body' countTypes={options?.countCharacters as string[]} />
     </div>
   }
 
   switch (style?.role) {
     case 'heading-1':
       return (
-        <>
-          <div draggable={false} contentEditable={editable} className="font-sans font-bold text-4xl pt-2 pb-4">
-            {children}
-            {options?.countCharacters && (
-              <div contentEditable={false} className='select-none text-xs text-gray-500 w-fit font-normal'>{`Antal tecken: ${Node.string(element).length}`}</div>
-            )}
-          </div>
-        </>
+        <div draggable={false} contentEditable={editable} className="font-sans font-bold text-4xl pt-2 pb-4">
+          {children}
+          <CharacterCount element={element} type='heading-1' countTypes={options?.countCharacters as string[]} />
+        </div>
       )
 
     case 'heading-2':
       return <div draggable={false} contentEditable={editable} className="font-sans font-bold text-xl py-2">
         {children}
+        <CharacterCount element={element} type='heading-2' countTypes={options?.countCharacters as string[]} />
       </div>
 
     case 'preamble':
       return <div draggable={false} contentEditable={editable} className="font-serif font-semibold py-2 dark:tracking-wide">
         {children}
+        <CharacterCount element={element} type='preamble' countTypes={options?.countCharacters as string[]} />
       </div>
 
     case 'vignette':
       return <div draggable={false} contentEditable={editable} className="font-sans font-bold py-1 text-sm uppercase">
         <span className="px-[0.4rem] py-[0.2rem] inline-block bg-slate-300 dark:text-slate-900">{children}</span>
+        <CharacterCount element={element} type='vignette' className='normal-case' countTypes={options?.countCharacters as string[]} />
       </div>
 
     default:
       return <div draggable={false} contentEditable={editable} className="font-serif opacity-80 italic line-through px-1 bg-red-200 dark:text-gray-700">
         {children}
+        <CharacterCount element={element} type='default' countTypes={options?.countCharacters as string[]} />
       </div>
   }
+}
+
+export const CharacterCount = ({ element, className, type, countTypes }: {
+  element: TBElement
+  type: string
+  countTypes: string[] | undefined
+  className?: string
+}) => {
+  const textLength = Node.string(element).length
+  if (Array.isArray(countTypes)
+    && countTypes.every(itm => typeof itm === 'string')
+    && countTypes?.includes(type)
+    && textLength > 0) {
+    return (
+      <div
+        contentEditable={false}
+        className={['select-none text-xs text-gray-500 w-fit font-normal font-sans', className].join(' ')}
+      >{`Antal tecken: ${textLength}`}
+      </div>
+  )
+  }
+  return <></>
 }
