4c6e15211c48ae2e8cc3f48941bed4dabbb53c89
Victor Lindh <victorlindh@hotmail.com>
2025-01-03 16:00:02 +0100

    simplify

--- lib/Text/components/index.tsx
+++ lib/Text/components/index.tsx
@@ -7,6 +7,7 @@ import { Node } from 'slate'
 export const Text = ({ children, element, options, rootNode }: Plugin.ComponentProps): JSX.Element => {
   const style = getTextStyles({}).find(t => t.role === element?.properties?.role)
   const editable = (TextbitElement.isElement(rootNode) && rootNode?.properties?.editable === false) ? false : undefined
+  const countCharacters = Array.isArray(options?.countCharacters) ? options.countCharacters as string[] : []
 
   if (style && !style.role) {
     // Regular body text
@@ -14,14 +15,14 @@ export const Text = ({ children, element, options, rootNode }: Plugin.ComponentP
       return (
         <div draggable={false} contentEditable={editable} className="font-sans dark:tracking-wide">
           {children}
-          <CharacterCount element={element} type='body' countTypes={options?.countCharacters as string[]} />
+          <CharacterCount element={element} type='body' countTypes={countCharacters} />
         </div>
       )
     }
 
     return <div draggable={false} contentEditable={editable} className="py-2 font-serif dark:tracking-wide">
       {children}
-      <CharacterCount element={element} type='body' countTypes={options?.countCharacters as string[]} />
+      <CharacterCount element={element} type='body' countTypes={countCharacters} />
     </div>
   }
 
@@ -30,32 +31,32 @@ export const Text = ({ children, element, options, rootNode }: Plugin.ComponentP
       return (
         <div draggable={false} contentEditable={editable} className="font-sans font-bold text-4xl pt-2 pb-4">
           {children}
-          <CharacterCount element={element} type='heading-1' countTypes={options?.countCharacters as string[]} />
+          <CharacterCount element={element} type='heading-1' countTypes={countCharacters} />
         </div>
       )
 
     case 'heading-2':
       return <div draggable={false} contentEditable={editable} className="font-sans font-bold text-xl py-2">
         {children}
-        <CharacterCount element={element} type='heading-2' countTypes={options?.countCharacters as string[]} />
+        <CharacterCount element={element} type='heading-2' countTypes={countCharacters} />
       </div>
 
     case 'preamble':
       return <div draggable={false} contentEditable={editable} className="font-serif font-semibold py-2 dark:tracking-wide">
         {children}
-        <CharacterCount element={element} type='preamble' countTypes={options?.countCharacters as string[]} />
+        <CharacterCount element={element} type='preamble' countTypes={countCharacters} />
       </div>
 
     case 'vignette':
       return <div draggable={false} contentEditable={editable} className="font-sans font-bold py-1 text-sm uppercase">
         <span className="px-[0.4rem] py-[0.2rem] inline-block bg-slate-300 dark:text-slate-900">{children}</span>
-        <CharacterCount element={element} type='vignette' className='normal-case' countTypes={options?.countCharacters as string[]} />
+        <CharacterCount element={element} type='vignette' className='normal-case' countTypes={countCharacters} />
       </div>
 
     default:
       return <div draggable={false} contentEditable={editable} className="font-serif opacity-80 italic line-through px-1 bg-red-200 dark:text-gray-700">
         {children}
-        <CharacterCount element={element} type='default' countTypes={options?.countCharacters as string[]} />
+        <CharacterCount element={element} type='default' countTypes={countCharacters} />
       </div>
   }
 }
@@ -63,12 +64,11 @@ export const Text = ({ children, element, options, rootNode }: Plugin.ComponentP
 export const CharacterCount = ({ element, className, type, countTypes }: {
   element: TBElement
   type: string
-  countTypes: string[] | undefined
+  countTypes: string[]
   className?: string
 }) => {
   const textLength = Node.string(element).length
-  if (Array.isArray(countTypes)
-    && countTypes.every(itm => typeof itm === 'string')
+  if (countTypes.every(itm => typeof itm === 'string')
     && countTypes?.includes(type)
     && textLength > 0) {
     return (
