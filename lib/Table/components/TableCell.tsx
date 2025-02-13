a40106195cd05fe100f501ce7b90340a735af35a
Danne Lundqvist <danne@dotvoid.com>
2025-02-13 17:06:17 +0100

    Support colspan and rowspan on table cells

--- lib/Table/components/TableCell.tsx
+++ lib/Table/components/TableCell.tsx
@@ -1,7 +1,14 @@
-import { forwardRef, type PropsWithChildren } from 'react'
+import { Plugin } from '@ttab/textbit'
+import { forwardRef } from 'react'
 
-export const TableCell = forwardRef<HTMLTableCellElement, PropsWithChildren>(({ children }, ref) => (
-  <td ref={ref} className='py-2 px-2 border'>{children}</td>
-))
+export const TableCell = forwardRef<HTMLTableCellElement, Plugin.ComponentProps>(({children, element}, ref) => {
+  const {colspan, rowspan} = element?.properties || {}
+
+  return (
+    <td ref={ref} className="py-2 px-2 border" colSpan={colspan ? Number(colspan) : undefined} rowSpan={rowspan ? Number(rowspan) : undefined}>
+      {children}
+    </td>
+  )
+})
 
 TableCell.displayName = 'TableCell'
