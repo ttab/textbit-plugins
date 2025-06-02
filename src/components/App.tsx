21b8960568b327bf19b3c4afa14f9e00c8e01eb0
gvl <git@gula.dev>
2025-06-02 15:53:07 +0200

    fix(link): remove empty links
    - Autofocus the URL input when EditLink mounts for better usability.
    - Remove ReactEditor.focus call, which didn't work
    - Refactor onBlur to delete link if URL is empty, otherwise update node.
    - Pass `true` to isValidLink for stricter validation.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -100,7 +100,7 @@ function Editor({ initialValue }: {
         className="outline-none dark:text-slate-100"
         value={value}
         onChange={(value: Descendant[]) => {
-          console.log(value, null, 2)
+          console.log(JSON.stringify(value, null, 2))
           setValue(value)
         }}
       >
