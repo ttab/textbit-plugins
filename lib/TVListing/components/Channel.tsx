1e89623c7a01bd804fbe0a3003020f3d362ea154
Victor Lindh <37335077+Victor-Lindh@users.noreply.github.com>
2025-06-11 13:58:47 +0200

    TVListing: expand to use external component, add removal functionality (#428)
    * tvlisting: enable options, to pass external component for rendering
    
    * enable removal in TVListing via X-icon + minor UI improvements
    
    * tv-listing: set base properties in action handler

--- lib/TVListing/components/Channel.tsx
+++ lib/TVListing/components/Channel.tsx
@@ -1,10 +1,13 @@
+import type { Plugin } from "@ttab/textbit"
 import type { PropsWithChildren } from "react"
 
-export const Channel = ({ children }: PropsWithChildren) => {
+export const Channel = ({ options }: Plugin.ComponentProps) => {
+  const ChannelComponent = options?.channelComponent as React.ComponentType<PropsWithChildren> | undefined
+
   return (
     <div className="border-b">
       <div contentEditable={false} className="font-semibold">Kanal</div>
-      {children}
+      {ChannelComponent && <ChannelComponent />}
     </div>
   )
 }
