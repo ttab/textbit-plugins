196656304e7e006afa24522cd8b107719f1c6bd9
Danne Lundqvist <danne@dotvoid.com>
2025-12-11 14:04:07 +0100

    Use correct placeholders from options so they are the same as what is used for actions

--- lib/Text/index.tsx
+++ lib/Text/index.tsx
@@ -4,6 +4,21 @@ import { Text as TextComponent } from './components'
 import { getTextStyles } from './textStyles'
 
 export const Text: TBPluginInitFunction = (options) => {
+  const placeholder = (element: Element) => {
+    switch (element.properties?.role) {
+      case 'heading-1':
+        return (typeof options?.titleLabel === 'string')
+          ? options.titleLabel
+          : '¶'
+      case 'heading-2':
+        return (typeof options?.subTitleLabel === 'string')
+          ? options.subTitleLabel
+          : '¶'
+      default:
+        return '¶'
+    }
+  }
+
   return {
     class: 'text',
     name: 'core/text',
@@ -18,16 +33,5 @@ export const Text: TBPluginInitFunction = (options) => {
       }
     },
     actions: getTextStyles(options || {})
-  }
-}
-
-function placeholder(element: Element) {
-  switch (element.properties?.role) {
-    case 'heading-1':
-      return 'Title'
-    case 'heading-2':
-      return 'Heading'
-    default:
-      return '¶'
   }
 }
