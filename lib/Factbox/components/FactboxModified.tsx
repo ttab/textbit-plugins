4226727300a700f7c0c70c0bd5f8850cba7a9f20
vilindh <37335077+vilindh@users.noreply.github.com>
2026-02-17 13:22:00 +0100

    factbox: add locale option, default to english (#448)

--- lib/Factbox/components/FactboxModified.tsx
+++ lib/Factbox/components/FactboxModified.tsx
@@ -3,14 +3,15 @@ import { formatDate } from "../lib/formatDate"
 interface Modified {
   modified?: string | undefined | boolean | number
   modifiedLabel?: string
+  locale?: string
 }
 
-export const FactboxModified = ({ modified = '', modifiedLabel }: Modified) => {
+export const FactboxModified = ({ modified = '', modifiedLabel, locale = 'en' }: Modified) => {
   if (modified) {
-    const formatted = formatDate(modified as string)
+    const formatted = formatDate(modified as string, locale)
     return (
       <div className='opacity-70 font-semibold text-xs flex justify-between items-center gap-1'>
-        <div>{modifiedLabel ?? 'Original is from'}</div>
+        <div>{modifiedLabel ?? 'Original is from...'}</div>
         <div>{formatted}</div>
       </div>
     )
--- lib/Factbox/lib/formatDate.ts
+++ lib/Factbox/lib/formatDate.ts
@@ -1,14 +1,15 @@
-export const formatDate = (date?: string): string => {
+export const formatDate = (date?: string, locale?: string): string => {
   if (!date) {
     return ''
   }
+
   const dateObject: number = Number(new Date(date))
   const secondsDiff = Math.round((dateObject - Date.now()) / 1000)
   const unitsInSec = [60, 3600, 86400, 86400 * 7, 86400 * 30, 86400 * 365, Infinity]
   const units: string[] = ['second', 'minute', 'hour', 'day', 'week', 'month', 'year']
   const unitIndex = unitsInSec.findIndex((cutoff) => cutoff > Math.abs(secondsDiff))
   const divisor = unitIndex ? unitsInSec[unitIndex - 1] : 1
-  const rtf = new Intl.RelativeTimeFormat('sv', { numeric: 'auto' })
+  const rtf = new Intl.RelativeTimeFormat(locale || 'en', { numeric: 'auto' })
   // @ts-expect-error time format should work without typing
   const relativeTime = rtf.format(Math.floor(secondsDiff / divisor), units[unitIndex])
   return relativeTime
