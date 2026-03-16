7a48d115800fd3d8d019228f874d348495efd2ba
vilindh <victorlindh@hotmail.com>
2026-03-16 22:42:30 +0100

    print-text: enforce role, text fields in normalizer

--- lib/PrintText/lib/normalizePrintText.ts
+++ lib/PrintText/lib/normalizePrintText.ts
@@ -1,9 +1,36 @@
-import { Text, Transforms, type Editor, type NodeEntry } from "slate"
+import { Text, Transforms, type Editor, type NodeEntry, Node } from "slate"
 
 export const normalizePrintText = (editor: Editor, nodeEntry: NodeEntry): boolean | undefined => {
   const [node, path] = nodeEntry
 
   if ('type' in node && node?.type === 'tt/print-text' && Array.isArray(node.children)) {
+    const children = Array.from(Node.children(editor, path))
+
+    if (children.length < 2) {
+      let hasRole = false
+
+      for (const [child] of children) {
+        if (child.type === 'tt/print-text/role') hasRole = true
+      }
+
+      // text is always at index 0, role at index 1
+      const [addType, atPos] = !hasRole
+        ? ['tt/print-text/role', 1]
+        : ['tt/print-text/text', 0]
+
+      Transforms.insertNodes(
+        editor,
+        {
+          id: crypto.randomUUID(),
+          class: 'text',
+          type: addType,
+          children: [{ text: '' }]
+        },
+        { at: [...path, atPos] }
+      )
+      return true
+    }
+
     for (let i = 0; i < node.children.length; i++) {
       const child = node.children[i]
       const childPath = path.concat(i)
@@ -21,7 +48,7 @@ export const normalizePrintText = (editor: Editor, nodeEntry: NodeEntry): boolea
 
             if (filteredText !== originalText) {
               Transforms.insertText(editor, filteredText, { at: textPath })
-              return
+              return true
             }
           }
         }
