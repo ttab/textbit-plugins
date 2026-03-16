11a77c4c629d2873062f0d9570c87871071216d5
vilindh <victorlindh@hotmail.com>
2026-03-16 22:40:36 +0100

    print-text: add clear label for text field, minor style improvements for role

--- lib/PrintText/components/Text.tsx
+++ lib/PrintText/components/Text.tsx
@@ -2,6 +2,9 @@ import type { PropsWithChildren } from 'react'
 
 export const Text = ({ children }: PropsWithChildren) => {
   return (
-    <div>{children}</div>
+    <div className='border-t flex items-baseline pt-1'>
+      <span className='text-xs italic text-gray-600 pr-2 pl-1 py-1' contentEditable={false}>Text:</span>
+      <div className='pr-2 pl-1'>{children}</div>
+    </div>
   )
 }
