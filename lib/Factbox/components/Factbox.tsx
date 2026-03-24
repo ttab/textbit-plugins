1480b32c1764dfe215ef699e92aba35f676fdf6b
vilindh <victorlindh@hotmail.com>
2026-03-24 12:57:02 +0100

    Factbox: feature: replace current factbox with recent version

--- lib/Factbox/components/Factbox.tsx
+++ lib/Factbox/components/Factbox.tsx
@@ -1,5 +1,5 @@
-import type { TBComponentProps } from '@ttab/textbit'
-import { FilePen, MessageCircleWarning, SaveIcon, X } from 'lucide-react'
+import type { TBComponentProps, TBElement } from '@ttab/textbit'
+import { FilePen, MessageCircleWarning, SaveIcon, X, CloudCheckIcon } from 'lucide-react'
 import { FactboxModified } from './FactboxModified'
 import { FactboxHeaderItem } from './FactboxHeaderItem'
 import { type Descendant, Transforms } from 'slate'
@@ -17,13 +17,15 @@ export const Factbox = ({ children, element, options, editor }: TBComponentProps
     modifiedLabel,
     footerTitle,
     saveToArchiveLabel,
-    unsavedLabel
+    unsavedLabel,
+    replaceVersionLabel
   } =  options as {
     headerTitle?: string
     modifiedLabel?: string
     footerTitle?: string
     saveToArchiveLabel?: string
-    unsavedLabel?: string
+    unsavedLabel?: string,
+    replaceVersionLabel?: string
   }
 
   const MESSAGE = footerTitle ?? 'Changes in the factbox text only affects this article'
@@ -38,13 +40,19 @@ export const Factbox = ({ children, element, options, editor }: TBComponentProps
           className='flex justify-start items-center m-1 p-2 bg-slate-100 dark:bg-slate-700 border-b-2 border-slate-200 ps-0.5 pb-0.5 pt-px cursor-default'
           onMouseDown={(e) => { e.stopPropagation() }}
         >
-          <FactboxHeaderItem
-            title={unSaved ? UNSAVED_MESSAGE : MESSAGE}
-            icon={{
-              icon: MessageCircleWarning,
-              className: 'text-red-800 dark:text-red-500'
-            }}
-          />
+          {unSaved
+            ? (
+              <FactboxHeaderItem
+                title={UNSAVED_MESSAGE}
+                icon={{
+                  icon: MessageCircleWarning,
+                  className: 'text-red-800 dark:text-red-500'
+                }}
+              />
+              )
+            :
+            null
+            }
 
           {unSaved && options?.onSave
             ? (
@@ -68,11 +76,32 @@ export const Factbox = ({ children, element, options, editor }: TBComponentProps
                     icon: SaveIcon
                   }}
                 />
-                <div className='text-xs'>{UNSAVED_MESSAGE}</div>
               </>
             )
             : null
           }
+
+          {!unSaved && options?.onGetUpdatedVersion ? (
+            <FactboxHeaderItem
+              title={replaceVersionLabel ?? 'Replace with recent version'}
+              icon={{
+                icon: CloudCheckIcon,
+              }}
+              onMouseDown={(e) => {
+                e.preventDefault()
+                e.stopPropagation()
+
+                const onSuccess = (newElement: TBElement) => {
+                  const n = editor.children.findIndex((child: Descendant) => (child as TBElement).id === element.id)
+                  if (n > -1) {
+                    Transforms.removeNodes(editor as never, { at: [n] })
+                    Transforms.insertNodes(editor as never, { ...newElement, id: element.id as string } as never, { at: [n] })
+                  }
+                }
+                ;(options.onGetUpdatedVersion as (id: string, onSuccess: (newElement: TBElement) => void) => void)(original_id as string, onSuccess)
+              }}
+            />
+          ) : null}
 
           {!unSaved && options?.onEditOriginal && typeof options.onEditOriginal === 'function' && original_id
             ? <FactboxHeaderItem
