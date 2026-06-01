f05de23a67fb6cbd546c92735e1902e23e8a30da
vilindh <victorlindh@hotmail.com>
2026-06-01 10:52:50 +0200

    TvListing: remove time as mandatory field

--- lib/TVListing/components/Time.tsx
+++ lib/TVListing/components/Time.tsx
@@ -2,7 +2,7 @@ import type { TBComponentProps } from '@ttab/textbit'
 import { FieldRow } from './FieldRow'
 
 export const Time = ({ editor, element, children }: TBComponentProps) => (
-  <FieldRow editor={editor} element={element} label='Starttid' mandatory>
+  <FieldRow editor={editor} element={element} label='Starttid'>
     {children}
   </FieldRow>
 )
