db55f4afad0f8c910b17cead5b6b15c53ca86c00
vilindh <37335077+vilindh@users.noreply.github.com>
2026-06-04 09:42:20 +0200

    Link: open in new tab and confirm before open (#463)
    * remove effect with event listeners
    
    * prompt user before opening
    
    * remove unused state value
    
    * get label from options

--- lib/Link/components/Link.tsx
+++ lib/Link/components/Link.tsx
@@ -1,40 +1,23 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import type { TBComponentProps } from '@ttab/textbit'
 import { isValidLink } from '../../shared/isValidLink'
 
-export const Link = ({ children, element }: TBComponentProps) => {
+export const Link = ({ children, element, options }: TBComponentProps) => {
   const url: string = element.properties?.url as string || ''
-  const [isPressed, setIsPressed] = useState<boolean>(false)
   const [isHovering, setIsHovering] = useState<boolean>(false)
 
-  useEffect(() => {
-    const keyDownListener = (event: KeyboardEvent): void => {
-      if (isPressed !== (event.metaKey || event.ctrlKey)) {
-        setIsPressed(true)
-      }
-    }
-    window.document.addEventListener('keydown', keyDownListener)
-
-    const keyUpListener = (event: KeyboardEvent): void => {
-      if (isPressed !== (event.metaKey || event.ctrlKey)) {
-        setIsPressed(false)
-      }
-    }
-    window.document.addEventListener('keyup', keyUpListener)
-
-    return () => {
-      window.document.removeEventListener('keydown', keyDownListener)
-      window.document.removeEventListener('keyup', keyUpListener)
-    }
-  })
+  const linkWarningLabel = options?.linkWarningLabel || 'Open link in a new tab? Continue only if you trust the source!'
 
   return (
     <a
       className={`underline decoration-2 underline-offset-4 ${isValidLink(url) ? 'decoration-blue-300' : 'decoration-wavy decoration-red-400'}`}
       href={url}
       onClick={(event) => {
-        if (event.ctrlKey || event.metaKey) {
-          window.open(url, '_blank')
+        event.preventDefault()
+        const message = `${url}\n\n${linkWarningLabel}`
+
+        if (window.confirm(message)) {
+          window.open(url, '_blank', 'noopener,noreferrer')
         }
       }}
       onMouseEnter={() => {
@@ -46,7 +29,7 @@ export const Link = ({ children, element }: TBComponentProps) => {
       title={`${element.properties?.title || ''}`}
       style={{
         textDecorationStyle: isValidLink(url) ? 'solid' : 'wavy',
-        cursor: isHovering && isPressed ? 'pointer' : 'auto'
+        cursor: isHovering ? 'pointer' : 'auto'
       }}
     >
       {children}
